01a770443a9fb959a3dd13986c3ba55895fcaa2d
Edder Luan <eluann100@gmail.com>
2021-07-16 14:25:52 -0300

    Subtotal

--- src/main/java/com/projeto/Springboot/entities/Payment.java
+++ src/main/java/com/projeto/Springboot/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.projeto.Springboot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
@@ -22,6 +23,7 @@ public class Payment implements Serializable{
     private Long id;
     private Instant moment;
     
+    @JsonIgnore
     @OneToOne
     @MapsId
     private Order order;
