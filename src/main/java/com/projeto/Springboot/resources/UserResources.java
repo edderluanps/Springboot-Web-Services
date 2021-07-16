397996f68c8f9349c0a60ea736188f6b34904033
Edder Luan <eluann100@gmail.com>
2021-07-16 14:58:41 -0300

    user inserts

--- src/main/java/com/projeto/Springboot/resources/UserResources.java
+++ src/main/java/com/projeto/Springboot/resources/UserResources.java
@@ -2,13 +2,17 @@ package com.projeto.Springboot.resources;
 
 import com.projeto.Springboot.entities.User;
 import com.projeto.Springboot.services.UserServices;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -33,6 +37,13 @@ public class UserResources {
         
         return ResponseEntity.ok().body(obj);
         
+    }
+    
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{idUser}").buildAndExpand(obj.getIdUser()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
     
 }
